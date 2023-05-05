8ca2083a9a3f4c7066fac24207db82e7236ed80c
gretel-hans <adjeihansel@icloud.com>
2023-05-05 17:55:51 +0200

    .

--- src/mainProject/Main.java
+++ src/mainProject/Main.java
@@ -1,10 +1,21 @@
 package mainProject;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		Video v1= new Video("Video testo",2);
-		
+		Scanner st= new Scanner(System.in);
+		Scanner sd= new Scanner(System.in);
+
+		System.out.print("Inserisci il titolo del video");
+		String titolo=st.nextLine();
+		System.out.print("Inserisci la durata di:"+ titolo);
+		int durata=sd.nextInt();
+		Video v2= new Video (titolo, durata);
+		v2.play();
+
 		v1.diminuisciLuminosita();
 		v1.play();
 
